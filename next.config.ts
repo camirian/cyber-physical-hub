da6e913747203f80c1e71e1ed8e902e4c0aa6f4e
Caaren Amirian <camirian@outlook.com>
2026-03-14 23:58:57 -0700

    fix: adjust cache headers and add deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=0, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
